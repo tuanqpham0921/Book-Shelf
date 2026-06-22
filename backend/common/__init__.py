a7ef86851a1d9e6a1d22f10066cbcb010db6d7da
Tuan Pham <tuanqpham0921@gmail.com>
2026-06-22 18:11:50 -0500

    remove AppContext from init

--- backend/common/__init__.py
+++ backend/common/__init__.py
@@ -1,12 +1,10 @@
 from .operation import OperationResult, task
-from .context import AppContext
 from .setup_logging import setup_logging
 from .workflow import Workflow
 
 __all__ = [
     "OperationResult",
     "task",
-    "AppContext",
     "setup_logging",
     "Workflow",
 ]
